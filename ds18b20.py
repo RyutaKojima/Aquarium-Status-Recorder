63fdc593721bdc76df7e3742c78e70572af5ceb1
r-kojima <kojima@sailboat.co.jp>
2019-11-13 09:19:42 +0900

    bugfix

--- ds18b20.py
+++ ds18b20.py
@@ -19,8 +19,8 @@ class DS18B20:
         temp_val = sensorData.split('=')
         return round(float(temp_val[-1]) / 1000, 1)
     
-    def __get_sensor_raw():
+    def __get_sensor_raw(self):
         try:
-            return subprocess.check_output(['cat', SENSOR_INFO_FILE]).decode()
+            return subprocess.check_output(['cat', self.__sensorInfoFile]).decode()
         except:
             return None
